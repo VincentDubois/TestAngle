4d0a053db24d0bfb28986764bb02b63066956bea
Vincent Dubois <dubois.marseille@wanadoo.fr>
2016-12-08 15:18:07 +0100

    Correction des permissions

--- app/src/main/java/com/example/vincentdubois/testangle/MainActivity.java
+++ app/src/main/java/com/example/vincentdubois/testangle/MainActivity.java
@@ -2,36 +2,58 @@ package com.example.vincentdubois.testangle;
 
 
 // Source : http://stackoverflow.com/questions/20339942/get-device-angle-by-using-getorientation-function
+// http://www.techrepublic.com/article/pro-tip-use-android-sensors-to-detect-orientation-changes/
 
+import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Surface;
+import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager mSensorManager;
-    Sensor accelerometer;
-    Sensor magnetometer;
+    Sensor mRotationSensor;
+
     float[] orientation = new float[3];
 
+    private static final int SENSOR_DELAY = 500 * 1000; // 500ms
+    private static final int FROM_RADS_TO_DEGS = -57;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Register the sensor listeners
-        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        try {
+            mSensorManager = (SensorManager) getSystemService(Activity.SENSOR_SERVICE);
+            mRotationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+            mSensorManager.registerListener(this, mRotationSensor, SENSOR_DELAY);
+        } catch (Exception e) {
+            Toast.makeText(this, "Hardware compatibility issue", Toast.LENGTH_LONG).show();
+        }
+
+        Display display = ((WindowManager) getSystemService(WINDOW_SERVICE))
+                .getDefaultDisplay();
+
+        int orientation = display.getRotation();
+
+        if (orientation == Surface.ROTATION_180) {
+            Toast.makeText(this, "180", Toast.LENGTH_LONG).show();
+        }
+
+
     }
 
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
-        mSensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI);
+//        mSensorManager.registerListener(this, mRotationSensor, SENSOR_DELAY);
     }
 
     protected void onPause() {
@@ -39,30 +61,40 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         mSensorManager.unregisterListener(this);
     }
 
+
     public void onAccuracyChanged(Sensor sensor, int accuracy) {  }
 
-    float[] mGravity;
-    float[] mGeomagnetic;
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
-            mGravity = event.values;
-        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
-            mGeomagnetic = event.values;
-        if (mGravity != null && mGeomagnetic != null) {
-            float R[] = new float[9];
-            float I[] = new float[9];
-            boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
-            if (success) {
-                SensorManager.getOrientation(R, orientation);
 
-                update();
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor == mRotationSensor) {
+            if (event.values.length > 4) {
+                float[] truncatedRotationVector = new float[4];
+                System.arraycopy(event.values, 0, truncatedRotationVector, 0, 4);
+                update(truncatedRotationVector);
+            } else {
+                update(event.values);
             }
         }
     }
 
+    private void update(float[] vectors) {
+        float[] rotationMatrix = new float[9];
+        SensorManager.getRotationMatrixFromVector(rotationMatrix, vectors);
+        int worldAxisX = SensorManager.AXIS_X;
+        int worldAxisZ = SensorManager.AXIS_Z;
+        float[] adjustedRotationMatrix = new float[9];
+        SensorManager.remapCoordinateSystem(rotationMatrix, worldAxisX, worldAxisZ, adjustedRotationMatrix);
+        SensorManager.getOrientation(adjustedRotationMatrix, orientation);
+
+        update();
+
+    }
+
+
     private void update() {
-        ((TextView)findViewById(R.id.textViewX)).setText("X "+orientation[0]);
-        ((TextView)findViewById(R.id.textViewY)).setText("Y "+orientation[1]);
-        ((TextView)findViewById(R.id.textViewZ)).setText("Z "+orientation[2]);
+        ((TextView)findViewById(R.id.textViewX)).setText("X "+(orientation[0]* FROM_RADS_TO_DEGS));
+        ((TextView)findViewById(R.id.textViewY)).setText("Y "+(orientation[1]* FROM_RADS_TO_DEGS));
+        ((TextView)findViewById(R.id.textViewZ)).setText("Z "+(orientation[2]* FROM_RADS_TO_DEGS));
     }
 }
